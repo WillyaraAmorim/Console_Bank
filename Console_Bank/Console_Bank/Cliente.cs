33aae8d39e58f3188886ea60da2900f1bd1999e4
Willyara Amorim <willyara-dv@hotmail.com>
2021-10-15 20:26:36 -0300

    Conta Corrente finalizada

---
+++ Console_Bank/Console_Bank/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Bank
+{
+    class Cliente
+    {
+        public string nome;
+        public string CPF;
+        public string profissao;
+
+        //método construtor
+        public Cliente (string nome, string CPF, string profissao)
+        {
+            this.setNome(nome);
+            this.setCPF(CPF);
+            this.setProfissao(profissao);
+        }
+
+        public string getName()
+        {
+            return this.nome;
+        }
+
+        public void setNome (string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getCPF ()
+        {
+            return this.CPF;
+        }
+
+        public void setCPF (string CPF)
+        {
+            this.CPF = CPF;
+        }
+
+        public string getProfissao ()
+        {
+            return this.profissao;
+        }
+
+        public void setProfissao (string profissao)
+        {
+            this.profissao = profissao;
+        }
+    }
+}
--- Console_Bank/Console_Bank/Program.cs
+++ Console_Bank/Console_Bank/Program.cs
@@ -6,7 +6,41 @@ namespace Console_Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cliente cliente = new Cliente("Willyara Amorim", "000.000.000-00", "Desenvolvedora");
+
+          /*Código inicial inutilizado após o método construtor
+           *cliente.setNome ();
+           *cliente.setCPF ();
+           *cliente.setProfissao ();*/
+
+            ContaCorrente conta = new ContaCorrente(cliente, 01, 0001);
+            /*conta.titular = c;
+            conta.agencia = 01;
+            conta.conta = 0001;*/
+
+            Console.WriteLine("Conta Corrente Criada!");
+            Console.WriteLine("Titular: {0}", conta.getTitular().getName());
+            Console.WriteLine("Agencia: {0}", conta.getAgencia());
+            Console.WriteLine("Conta: {0}", conta.getConta());
+
+            Console.WriteLine("Parabéns! Seu saldo é de {0}, obrigado por criar a sua conta", conta.getSaldo());
+
+            double valorDeposito = 100;
+            conta.Depositar(valorDeposito);
+            Console.WriteLine ("Valor depositado! O Seu saldo é de {0}", conta.getSaldo());
+
+            double valorSaque = 200;
+            conta.Sacar(valorSaque);
+            Console.WriteLine("Seu saldo é de {0}", conta.getSaldo());
+
+            Cliente clienteVitor = new Cliente("Vitor David", "0000000000", "UX Designer");
+            ContaCorrente contaVitor = new ContaCorrente(clienteVitor, 02, 0002);
+            double valorPix = 100;
+            conta.Pix(valorPix, contaVitor);
+
+            Console.WriteLine("Seu saldo é de R${0}", contaVitor.getSaldo());
+            Console.Write("Seu saldo é de R${0}",contaVitor.getSaldo());
+        
         }
     }
 }
